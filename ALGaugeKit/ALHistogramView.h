e7d5ddb2a15fd48a9ca9b4b425a5675f0b597ffc
Alf Watt <alf@istumbler.net>
2016-02-22 17:37:47 -0800

    Histogram view class

---
+++ ALGaugeKit/ALHistogramView.h
@@ -0,0 +1,19 @@
+//
+//  ALHistogramView.h
+//  ALGaugeKit
+//
+//  Created by Alf Watt on 2/22/16.
+//  Copyright © 2016 AutomaticLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ALBorderedView.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ALHistogramView : ALBorderedView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ALGaugeKit/ALHistogramView.m
@@ -0,0 +1,14 @@
+//
+//  ALHistogramView.m
+//  ALGaugeKit
+//
+//  Created by Alf Watt on 2/22/16.
+//  Copyright © 2016 AutomaticLabs. All rights reserved.
+//
+
+#import "ALHistogramView.h"
+
+@implementation ALHistogramView
+
+
+@end
